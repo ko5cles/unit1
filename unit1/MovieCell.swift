57a8551b83c315bfbb8474e703d1042cdd9a96b0
koppppp <88808900+koppppp@users.noreply.github.com>
2021-10-01 20:30:04 -0400

    unit2

---
+++ unit1/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  unit1
+//
+//  Created by Ze Pan on 9/25/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var UIImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ unit1/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  unit1
+//
+//  Created by Ze Pan on 10/1/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
